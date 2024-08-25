11be534c1994ba2133f5fa748e2fa313fc1faef3
AgFelix <marcelo12543@hotmail.com>
2024-08-25 18:51:22 -0300

    add: background fundo

--- src/components/CategoryCarousel/index.jsx
+++ src/components/CategoryCarousel/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Carousel from 'react-elastic-carousel'
 import Category from '../../assets/category.png'
-import { Container, CategoryImg } from './styles'
+import { Container, CategoryImg, ContainerItens, Image, Button} from './styles'
 import api from '../../services/api'
 
 export function CategoryCarousel() {
@@ -9,25 +9,34 @@ export function CategoryCarousel() {
     useEffect(() => {
 
         async function loadCategories() {
-            const response = await api.get('categories')
+            const {data} = await api.get('categories')
 
-            setCategories()
+            setCategories(data)
         }
 
         loadCategories()
     }, [])
 
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+        { width: 850, itemsToShow: 3 },
+        { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+        { width: 1450, itemsToShow: 5 },
+        { width: 1750, itemsToShow: 6 },
+    ]
+
     return (
         <Container>
             <CategoryImg src={Category} alt="logo da categoria" />
 
-            <Carousel itemsToShow={4}>
+            <Carousel itemsToShow={6} style={{width:'90'}} breakPoints={breakPoints} >
            {
             categories && categories.map(category => (
-                <div key={category.id}>
-                    <img src={category.url} alt="foto da categoria"></img>
-                    <button>{category.name}</button>
-                </div>
+                <ContainerItens key={category.id}>
+                    <Image src={category.url} alt="foto da categoria"></Image>
+                    <Button>{category.name}</Button>
+                </ContainerItens>
             ))
            }
             </Carousel>
--- src/components/CategoryCarousel/styles.js
+++ src/components/CategoryCarousel/styles.js
@@ -1,7 +1,70 @@
 import styled from "styled-components";
+import backgroundImage from '../../assets/BG-FUNDO.svg'
 
 export const Container = styled.div`
+background-image: url(${backgroundImage});
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 35px;
+padding: 35px 0;
+
+.rec.rec-arrow {
+    background-color: #9758a6;
+    color: white;
+
+    &:hover {
+        border: 1px solid #9758a6;
+        background-color: #efefef;
+        color: #9758a6;;
+    }
+
+    &:disabled {
+        border: none;
+        background-color: #bebebf;
+        color: #efefef;
+    
+    }
+}
+
+
 `
 export const CategoryImg = styled.img`
-width: 100%;
+width: 20%;
+
+`
+
+export const Button = styled.button `
+color: white;
+margin-top: 16px;
+background: #9758a6;
+border-radius: 8px;
+height: 50px;
+border: none;
+
+font-size: normal;
+font-weight: bold;
+font-size: 18x;
+line-height: 100%;
+text-align: center;
+
+cursor: pointer;
+
+&:hover {
+    opacity: 0.8;
+}
+
+&:active {
+    opacity: 0.6;
+}
+`
+
+export const ContainerItens = styled.div `
+display: flex;
+flex-direction: column;
+`
+
+export const Image = styled.div `
+width: 200px;
+border-radius: 10px;
 `
